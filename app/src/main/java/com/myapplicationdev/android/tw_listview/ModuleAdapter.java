bd1d6d9c6bfea47d6017dad8c253796a6198589b
LI SHUFANG <lishufang1121@gmail.com>
2021-04-22 15:53:16 +0800

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	.idea/misc.xml

--- app/src/main/java/com/myapplicationdev/android/tw_listview/ModuleAdapter.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/ModuleAdapter.java
@@ -2,11 +2,13 @@ package com.myapplicationdev.android.tw_listview;
 
 import android.content.Context;
 
+import java.util.ArrayList;
+
 public class ModuleAdapter {
 
     private Context context;
 
 
-
-
+    public ModuleAdapter(SecondActivity secondActivity, int module_row, ArrayList<Course> course) {
+    }
 }

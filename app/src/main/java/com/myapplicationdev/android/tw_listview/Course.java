53522dd52f588c40b78ea8593b20b20894a98971
LI SHUFANG <lishufang1121@gmail.com>
2021-04-22 15:44:45 +0800

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	.idea/misc.xml

--- app/src/main/java/com/myapplicationdev/android/tw_listview/Course.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/Course.java
@@ -3,29 +3,30 @@ package com.myapplicationdev.android.tw_listview;
 
 public class Course {
 
-    private int year;
-    private boolean moduleCode;
-    private int imageId
+    private String year;
+    private String moduleCode;
+    private int imageId;
 
-    public Course(int year, boolean moduleCode, int imageId) {
+
+    public Course(String year, String moduleCode, int imageId) {
         this.year = year;
         this.moduleCode = moduleCode;
         this.imageId = imageId;
     }
 
-    public int getYear() {
+    public String getYear() {
         return year;
     }
 
-    public void setYear(int year) {
+    public void setYear(String year) {
         this.year = year;
     }
 
-    public boolean isModuleCode() {
+    public String getModuleCode() {
         return moduleCode;
     }
 
-    public void setModuleCode(boolean moduleCode) {
+    public void setModuleCode(String moduleCode) {
         this.moduleCode = moduleCode;
     }
 

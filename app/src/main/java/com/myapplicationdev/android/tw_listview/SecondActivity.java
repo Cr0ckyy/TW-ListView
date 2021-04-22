47af7261e0d0ce63d8754f385028ead367047872
LI SHUFANG <lishufang1121@gmail.com>
2021-04-22 15:49:38 +0800

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	.idea/misc.xml

--- app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
@@ -3,14 +3,18 @@ package com.myapplicationdev.android.tw_listview;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
 
     ListView lv;
     TextView tvYear;
-
+    ArrayAdapter aa;
+    ArrayList<Course> course;
 
 
     @Override
@@ -24,6 +28,26 @@ public class SecondActivity extends AppCompatActivity {
         Intent i = getIntent();
         String year = i.getStringExtra("year");
         tvYear.setText(year);
+
+
+        lv = (ListView) this.findViewById(R.id.lv);
+
+        // Create a few food objects in Food array
+        course = new ArrayList<course>();
+        course.add(new Course("Year 1", "C111", 1));
+        course.add(new Course("Year 1", "C222", 2));
+        course.add(new Course("Year 1", "C333", 3));
+        course.add(new Course("Year 1", "C444", 1));
+        course.add(new Course("Year 1", "C555", 2));
+        course.add(new Course("Year 1", "C666", 3));
+        course.add(new Course("Year 1", "C777", 1));
+        course.add(new Course("Year 1", "C888", 2));
+        course.add(new Course("Year 1", "C999", 3));
+
+        // Link this Activity object, the row.xml layout for
+        //  each row and the food String array together
+        aa = new ModuleAdapter(this, R.layout.module_row, course);
+        lv.setAdapter(aa);
 
 
     }
